02ef7deb5517108f0964bd588c026a414cfff2d0
Dinh Phuc Tuan Nhat <nhat18349276@gmail.com>
2025-04-22 03:58:14 +0700

    first commit

---
+++ app/src/main/java/com/example/firebasecrud/course.kt
@@ -0,0 +1,11 @@
+package com.example.firebasecrud
+
+data class Course(
+    // on below line creating variables.
+    var courseName: String? = "",
+    var courseDuration: String? = "",
+    var courseDescription: String? = ""
+
+
+
+)
